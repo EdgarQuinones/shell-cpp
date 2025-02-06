fd9de0457159d5483d95c03c98cc0a7fd9fba4f2
Edgar Quinones <quinones_edgar@proton.me>
2025-02-06 16:49:04 -0500

    Added type command

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,25 @@
 
 using namespace std;
 
-string commands[3] = {
+// commands
+const string commands[3] = {
   "exit 0",
   "echo",
   "type"
 };
 
+// Common string outputs
+const string shellStart("$ ");
+const string inputError(": command not found");
+const string typeError(": not found");
+const string typeSuccess(" is a shell builtin");
+
 //functions
 void echo(string input);
+void type(string input);
 
 int main() {
 
-  // Common String outputs
-  const string invalidCommand(": command not found");
 
   // Flush after every std::cout / std:cerr
   cout << unitbuf;
@@ -23,7 +29,7 @@ int main() {
   // Read-Eval-Print Loop
   while(true) {
 
-    std::cout << "$ ";
+    std::cout << shellStart;
 
     string input;
     getline(cin, input);
@@ -36,15 +42,32 @@ int main() {
     else if (command == commands[1]) {
         echo(input);
     }
+    else if (command == commands[2]) {
+        type(input);
+    }
     else {
-        cout << input << invalidCommand << endl;
+        cout << input << inputError << endl;
     }
 
   }
 
 }
 
-void echo(string str) {
-   string echoInput(str.substr(str.find(" ") + 1, str.length() - 1));
+void echo(string input) {
+   string echoInput(input.substr(input.find(" ") + 1, input.length() - 1));
    cout << echoInput << endl;
+}
+
+// Only checks the first string up until the space
+void type(string input) {
+   string secondString(input.substr(input.find(" ") + 1, input.length() - 1));
+   string trimmedSecondString(secondString.substr(0, secondString.find(" ")));
+  for (int i = 0; i < sizeof(commands); i++) {
+    string trimmedCommand(commands[i].substr(0, commands[i].find(" ")));
+    if (trimmedSecondString == trimmedCommand) {
+        cout << trimmedCommand << typeSuccess << endl;
+        return;
+    }
+  }
+  cout << trimmedSecondString << typeError << endl;
 }
